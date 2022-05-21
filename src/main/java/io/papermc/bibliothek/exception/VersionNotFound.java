3170bd4f857c74782e3f24610943417291247431
Riley Park <riley.park@meino.net>
2022-05-21 05:03:17 -0700

    2022

--- src/main/java/io/papermc/bibliothek/exception/VersionNotFound.java
+++ src/main/java/io/papermc/bibliothek/exception/VersionNotFound.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of bibliothek, licensed under the MIT License.
  *
- * Copyright (c) 2019-2021 PaperMC
+ * Copyright (c) 2019-2022 PaperMC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
