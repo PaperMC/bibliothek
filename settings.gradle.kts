0ee88af1dcccb1fa423fc6c834cc6ddbce107415
Riley Park <rileysebastianpark@gmail.com>
2023-06-07 21:47:39 -0700

    chore: update dependencies fixes #132 fixes #140 fixes #141 fixes #144 fixes #145

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
-rootProject.name = "bibliothek"
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
 
-enableFeaturePreview("VERSION_CATALOGS")
+rootProject.name = "bibliothek"
