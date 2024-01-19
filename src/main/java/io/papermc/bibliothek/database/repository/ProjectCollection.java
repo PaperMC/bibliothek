5362e4958cdad9b9d4fe090b30845b788f830c4a
Riley Park <rileysebastianpark@gmail.com>
2024-01-18 21:54:26 -0800

    2024

--- src/main/java/io/papermc/bibliothek/database/repository/ProjectCollection.java
+++ src/main/java/io/papermc/bibliothek/database/repository/ProjectCollection.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of bibliothek, licensed under the MIT License.
  *
- * Copyright (c) 2019-2023 PaperMC
+ * Copyright (c) 2019-2024 PaperMC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
