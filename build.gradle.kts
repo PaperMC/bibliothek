4efb5126b69b00693a823f308e58ebef51ddb43a
MiniDigger | Martin <admin@benndorf.dev>
2024-05-09 12:32:57 +0200

    feat: add actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,6 +96,7 @@ dependencies {
   implementation("org.springframework.boot", "spring-boot-starter-data-mongodb")
   implementation("org.springframework.boot", "spring-boot-starter-validation")
   implementation("org.springframework.boot", "spring-boot-starter-web")
+  implementation("org.springframework.boot", "spring-boot-starter-actuator")
   testImplementation("org.springframework.boot", "spring-boot-starter-test") {
     exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
   }
